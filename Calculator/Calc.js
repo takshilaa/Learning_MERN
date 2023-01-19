41761572bec0774f1cbc19e928a6e09c4fa4634d
rohhhit31 <rohitbeni31@gmail.com>
2023-01-19 21:44:23 +0530

    Express JS

--- Calculator/Calc.js
+++ Calculator/Calc.js
@@ -2,7 +2,7 @@ const express= require("express");
 const bodyParser = require("body-parser");
 
 const app =  express();
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({extended:true}));
 app.get("/", function(req , res){
 // res.sendFile(__dirname+"index.html");
 res.sendFile(__dirname + "/index.html");
@@ -16,5 +16,17 @@ app.post("/", function(req, res){
      var ressult=num1+num2;
 res.send("Result is : "+ ressult);
 })
+
+app.get("/bmi", function(req,res){
+     res.sendFile(__dirname+"/bmi.html");
+});
+
+
+app.post("/bmi", function(req, res){
+     var Weight= parseFloat(req.body.Weight);
+     var Height= parseFloat(req.body.Height);
+     var ressult2=Weight/(Height*Height);
+res.send("Result is : "+ ressult2);
+});
 
 app.listen(3000,function(){console.log("Server Started ")});
